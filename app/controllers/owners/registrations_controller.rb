675ba7fd1c40bad4b82f3bff66a6743f8769596a
vrs-git <vrs1124@gmail.com>
2018-01-01 14:34:24 +0530

    controller removed

--- app/controllers/owners/registrations_controller.rb
+++ app/controllers/owners/registrations_controller.rb
@@ -1,13 +1,5 @@
 class Owners::RegistrationsController < Devise::RegistrationsController
    
-    protected
-
-    def after_sign_in_path_for(resource_or_scope)
-    
-    dashboard_path 
-  
-  end
-end
 
 
 
