b4a9763317348026c761231f9f1078b8f5512ecb
vrs-git <vrs1124@gmail.com>
2018-01-05 15:00:53 +0530

    incomplete_interface

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,10 +7,12 @@ before_action :authenticate_user!, only: [:dashboard]
     def index
 
       @categories = Category.all
+      @products = Product.all
 
 	end
     
     def dashboard
+
 
     end
 
